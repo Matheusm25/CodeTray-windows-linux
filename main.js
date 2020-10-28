4389c4372de45f95366cde664d34b05ff7e61967
Matheus Marques <matheusml25@hotmail.com>
2020-10-28 20:50:38 -0300

    prompt for project name

--- main.js
+++ main.js
@@ -1,103 +1,123 @@
 const { resolve, basename } = require('path');
-const { app, Tray, Menu, dialog, MenuItem } = require('electron');
+const {
+  app,
+  Tray,
+  Menu,
+  dialog,
+  MenuItem,
+  BrowserWindow,
+} = require('electron');
 const Store = require('electron-store');
 const spawn = require('cross-spawn');
 const { exec } = require('child_process');
 
-let tray = null;
+const prompt = require('electron-prompt');
+
+const tray = null;
 const fs = require('fs');
 
 const schema = {
-    projects: {
-        type: 'string'
-    }
-}
+  projects: {
+    type: 'string',
+  },
+};
 
 let mainTray = {};
+let browserWindowToSuport;
 
 const store = new Store({ schema });
 
 function render(tray = mainTray) {
-    const storedProjects = store.get('projects');
-    const projects = storedProjects ? JSON.parse(storedProjects) : [];
+  const storedProjects = store.get('projects');
+  const projects = storedProjects ? JSON.parse(storedProjects) : [];
 
-    const items = projects.map(( { name, path } ) => ({
-        label: name,
-        submenu: [
-            {
-                label: 'Abrir no VSCode',
-                click: () => {
-                    spawn('code', [path]);
-                },
-            },
-            {
-                label: 'Abrir no terminal',
-                click: () => {
-                    exec('gnome-terminal --working-directory="' + [path] + '"');
-                },
-            },
-            {
-                label: 'Abrir no Explorer',
-                click: () => {
-                    var x = exec('nemo ' + [path]);
-                },
-            },
-            {
-                label: 'Remover',
-                click: () => {
-                    store.set('projects', JSON.stringify(projects.filter(item => item.path !== path)));
-                    render();
-                },
-            },
-        ],
-    }));
-
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Adicionar novo projeto...',
-            click: () => {
-                const result = dialog.showOpenDialog({ properties: [ 'openDirectory']});
-
-                if (!result) return;
-
-                const [ path ] = result;
-                const name = basename(path);
-
-                store.set(
-                    'projects',
-                    JSON.stringify([
-                        ...projects,
-                        {
-                            path,
-                            name,
-                        },
-                    ]),
-                );
-
-                render();
-            },
+  const items = projects.map(({ name, path }) => ({
+    label: name,
+    submenu: [
+      {
+        label: 'Abrir no VSCode',
+        click: () => {
+          spawn('code', [path]);
         },
-        {
-            type: 'separator',
+      },
+      {
+        label: 'Abrir no terminal',
+        click: () => {
+          exec(`gnome-terminal --working-directory="${[path]}"`);
         },
-        ...items,
-        {
-            type: 'separator',
+      },
+      {
+        label: 'Abrir no Explorer',
+        click: () => {
+          const x = exec(`nemo ${[path]}`);
         },
-        {
-            type: 'normal',
-            label: 'Fechar Code Tray',
-            role: 'quit',
-            enabled: true,
+      },
+      {
+        label: 'Remover',
+        click: () => {
+          store.set('projects', JSON.stringify(projects.filter(item => item.path !== path)));
+          render();
         },
-    ]);
+      },
+    ],
+  }));
 
-    tray.setContextMenu(contextMenu);
-    tray.on('click', tray.popUpContextMenu);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Adicionar novo projeto...',
+      click: async () => {
+        const result = dialog.showOpenDialog({ properties: ['openDirectory'] });
+
+        const projectName = await prompt({
+          title: 'Project name',
+          // label: 'Project name',
+          inputAttrs: {
+            type: 'text',
+          },
+          type: 'input',
+          height: 120,
+        }, browserWindowToSuport);
+
+        if (!result) return;
+
+        const [path] = result;
+        const name = basename(path);
+
+        store.set(
+          'projects',
+          JSON.stringify([
+            ...projects,
+            {
+              path,
+              name: projectName || name,
+            },
+          ]),
+        );
+
+        render();
+      },
+    },
+    {
+      type: 'separator',
+    },
+    ...items,
+    {
+      type: 'separator',
+    },
+    {
+      type: 'normal',
+      label: 'Fechar Code Tray',
+      role: 'quit',
+      enabled: true,
+    },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.on('click', tray.popUpContextMenu);
 }
 
 app.on('ready', () => {
-    mainTray = new Tray(resolve(__dirname, 'assets', 'iconWhiteTemplate.png'));
-
-    render(mainTray);
+  browserWindowToSuport = new BrowserWindow({ show: false });
+  mainTray = new Tray(resolve(__dirname, 'assets', 'iconWhiteTemplate.png'));
+  render(mainTray);
 });
